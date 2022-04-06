36b1e04248e1e9b1ab34ef14bb06a022ca3c7ef8
MrAnt1vit <vityuka05@mail.ru>
2022-04-06 21:13:28 +0300

    таблица юзеров

---
+++ data/users.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    class_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    tasks = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+
+def set_password(self, password):
+    self.hashed_password = generate_password_hash(password)
+
+
+def check_password(self, password):
+    return check_password_hash(self.hashed_password, password)
